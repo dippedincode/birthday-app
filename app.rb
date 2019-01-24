5ad0ed0f253520d36f3d571cc1b59db8cf62e6ed
Diptis Halder <diptis.halder@gmail.com>
2019-01-24 12:07:49 +0000

    v1.1 Created /brithday route for page to show form or name passed in the form

--- app.rb
+++ app.rb
@@ -5,3 +5,14 @@ set :session_secret, 'super secret'
 get '/' do
   'Hello world! I did it my way.'
 end
+
+get '/birthday' do
+  @name = params[:name]
+  erb(:index)
+end
+
+post '/birthday' do
+  @name = params[:name]
+  erb(:index)
+end
+
